36d2cab9d66299374ef82b290884f345a7f06939
z-r0crypt <31608394+z-r0crypt@users.noreply.github.com>
2020-05-21 08:23:40 -0400

    Create rapidns.sh

---
+++ rapidns.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Usage : ./rapidns.sh <target.domain>
+
+curl -s "https://rapiddns.io/subdomain/$1?full=1#result" | grep "<td><a" | cut -d '"' -f 2 | grep http | cut -d '/' -f3 | sed 's/#results//g' | sort -u
